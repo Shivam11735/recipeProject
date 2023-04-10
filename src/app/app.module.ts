3f706a7e3017eb9ba0dd51387053b230f9beda36
Shivam11735 <sganguly2096@gmail.com>
2023-04-10 16:08:08 +0530

    save successful

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { GlobalHeader } from './globalHeader/globalHeader.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeDetail } from './recipe/recipeDetail/recipeDetail.component';
-import { RecipeItem } from './recipe/recipeItem/recipeItem.component';
+import { RecipeItem } from './recipe/recipeList/recipeItem/recipeItem.component';
 import { RecipeList } from './recipe/recipeList/recipeList.component';
-import { ShoppingList } from './shopping/shoppingList/shoppingList.component';
-import { ShoppingListEdit } from './shopping/shoppingListEdit/shoppingListEdit.component';
+import { ShoppingList } from './shoppingList/shoppingList.component';
+import { ShoppingListEdit } from './shoppingList/shoppingListEdit/shoppingListEdit.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { ShoppingConponent } from './shopping/shopping.component';
 
